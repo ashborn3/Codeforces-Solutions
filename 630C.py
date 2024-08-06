ead12aa4f39680fd5a6c6078d3743798b2adc03c
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-08-06 15:13:32 +0530

    solved 630C question 4

---
+++ 630C.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+# Take int input
+def inp():
+    return(int(input()))
+# Take list input
+def inlt():
+    return(list(map(int,input().split())))
+# Take string input as a array of chars
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+# For taking space seperated integer variable inputs.
+def invr():
+    return(map(int,input().split()))
+
+
+n = inp()
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + 2**i
+print(sum)
